493d6768ce92ab2b82e05639b8510ea61e686433
Your Name <you@example.com>
2024-04-29 02:29:49 -0300

    first commit

--- back-end/app.py
+++ back-end/app.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 app = FastAPI()
 
+# Configuração do CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+    allow_origins=["http://localhost:5173"]
+)
+
 @app.get('/get_data')
 async def get_data():
-    return {'body': 'renderizar esse texto'}
-
+    return {'body': 'um tomate pequeno'}
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=7777)
+
